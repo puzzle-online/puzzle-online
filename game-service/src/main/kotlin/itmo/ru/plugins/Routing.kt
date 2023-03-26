82f2e68373bb833c8409e72693588218f03e0fb9
RuslanZaripov <zaripovruslan864@gmail.com>
2023-03-26 11:21:57 +0300

    added method name for connection message

--- game-service/src/main/kotlin/itmo/ru/plugins/Routing.kt
+++ game-service/src/main/kotlin/itmo/ru/plugins/Routing.kt
@@ -5,9 +5,16 @@ import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
 @Serializable
-data class Message(val sender: String, val content: String, val timestamp: String)
+data class Message(val method: METHOD, val data: String)
+
+enum class METHOD { CONNECT }
+
+@Serializable
+data class UUID(val uuid: String)
 
 fun getUUID() = java.util.UUID.randomUUID().toString()
 
@@ -15,11 +22,16 @@ fun Application.configureRouting() {
     routing {
         webSocket("/chat") {
             try {
-                sendSerialized(getUUID())
+
+                val clientID = UUID(getUUID())
+                val connectData = Json.encodeToString(clientID)
+                sendSerialized(Message(METHOD.CONNECT, connectData))
+
                 for (frame in incoming) {
                     val data = converter?.deserialize<Message>(frame)!!
                     sendSerialized(data)
                 }
+
             } catch (e: Exception) {
                 this@configureRouting.log.error("Error occurred in websocket", e)
             }
