1aa5d8d78776acc7abc7c7673aec2eea884593c4
KhoDis <khodisdev@gmail.com>
2023-03-26 21:50:20 +0300

    added playing

--- game-ui/src/components/HelloWorld.vue
+++ game-ui/src/components/HelloWorld.vue
@@ -5,11 +5,12 @@
       Current session: {{ clientId }}
     </div>
     <div>
-      Current game ID: {{ game.gameId }}, balls: {{ game.balls }}, players: {{ game.clients }}
+      Current game ID: {{ game.gameId }}, balls: {{ game.balls.length }}, players: {{ game.clients }}
     </div>
     <div v-for="(message, index) in messages" :key="index">
       {{ message }}
     </div>
+    <Ball v-for="ball in game.balls" :key="ball.ballId" :color="ball.color" />
     <form @submit.prevent="sendMessage">
       <input v-model="messageText" type="text" placeholder="Type your message here">
       <button type="submit">Send</button>
@@ -21,11 +22,28 @@
       <input v-model="gameJoinInput" type="text" placeholder="Type your message here">
       <button type="submit">Join Game</button>
     </form>
+    <form @submit.prevent="playRed">
+      <input v-model="playBallIdStr" type="text" placeholder="Type your message here">
+      <button type="submit">Make red</button>
+    </form>
+    <form @submit.prevent="playBlue">
+      <input v-model="playBallIdStr" type="text" placeholder="Type your message here">
+      <button type="submit">Make blue</button>
+    </form>
+    <form @submit.prevent="playGreen">
+      <input v-model="playBallIdStr" type="text" placeholder="Type your message here">
+      <button type="submit">Make green</button>
+    </form>
   </div>
 </template>
 
 <script>
+import Ball from './Ball.vue';
+
 export default {
+  components: {
+    Ball
+  },
   data() {
     return {
       ws: null,
@@ -34,6 +52,7 @@ export default {
       messages: [],
       clientId: '',
       game: {},
+      playBallIdStr: '0',
     };
   },
   mounted() {
@@ -57,6 +76,8 @@ export default {
         } else if (response.method === 'chat') {
           this.messages.push(`${response.message.sender}: ${response.message.content}`);
         } else if (response.method === 'join') {
+          this.game = response.game;
+        } else if (response.method === 'update') {
           this.game = response.game;
         } else {
           this.messages.push("Unknown message: " + JSON.stringify(response,null, 2));
@@ -92,6 +113,39 @@ export default {
         method: 'join',
         clientId: this.clientId,
         gameId: this.gameJoinInput,
+      }));
+    },
+    playRed() {
+      this.ws.send(JSON.stringify({
+        method: 'play',
+        clientId: this.clientId,
+        gameId: this.game.gameId,
+        ball: {
+          ballId: parseInt(this.playBallIdStr),
+          color: 'red',
+        }
+      }));
+    },
+    playBlue() {
+      this.ws.send(JSON.stringify({
+        method: 'play',
+        clientId: this.clientId,
+        gameId: this.game.gameId,
+        ball: {
+          ballId: parseInt(this.playBallIdStr),
+          color: 'blue',
+        }
+      }));
+    },
+    playGreen() {
+      this.ws.send(JSON.stringify({
+        method: 'play',
+        clientId: this.clientId,
+        gameId: this.game.gameId,
+        ball: {
+          ballId: parseInt(this.playBallIdStr),
+          color: 'green',
+        }
       }));
     },
   },
