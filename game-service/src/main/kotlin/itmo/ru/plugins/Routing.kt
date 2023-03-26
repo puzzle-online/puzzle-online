5b55934dfab81bb8d7e9b755431bf5edaf3c8b22
RuslanZaripov <zaripovruslan864@gmail.com>
2023-03-26 16:01:13 +0300

    fixed sending modified game state for all users

--- game-service/src/main/kotlin/itmo/ru/plugins/Routing.kt
+++ game-service/src/main/kotlin/itmo/ru/plugins/Routing.kt
@@ -1,18 +1,22 @@
 package itmo.ru.plugins
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import io.ktor.network.sockets.*
 import io.ktor.serialization.*
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 import io.ktor.websocket.*
+import io.ktor.websocket.serialization.*
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import java.util.*
+import java.util.concurrent.atomic.AtomicInteger
 
 @JvmInline
 @Serializable
 value class ClientId(val value: String)
+
 @JvmInline
 @Serializable
 value class GameId(val value: String)
@@ -20,10 +24,13 @@ value class GameId(val value: String)
 enum class METHOD {
     @SerialName("connect")
     CONNECT,
+
     @SerialName("chat")
     CHAT,
+
     @SerialName("create")
     CREATE,
+
     @SerialName("join")
     JOIN;
 
@@ -34,23 +41,21 @@ enum class METHOD {
 // TODO: think about sealed class
 @Serializable
 @JsonIgnoreProperties(ignoreUnknown = true)
-open class Transfer(val method: METHOD)
-
-
-
-@Serializable
-data class ClientIdTransfer(val clientId: ClientId) : Transfer(METHOD.CONNECT)
-
-@Serializable
-data class MessageTransfer(val message: Message) : Transfer(METHOD.CHAT)
-
-@Serializable
-data class GameTransfer(val game: Game) : Transfer(METHOD.CREATE)
-
+open class Response(val method: METHOD)
 
 
 @Serializable
-data class Game(val gameId: GameId, val balls: Int, val clients: MutableList<ClientId> = mutableListOf())
+data class ClientIdResponse(val clientId: ClientId) : Response(METHOD.CONNECT)
+
+@Serializable
+data class MessageResponse(val message: Message) : Response(METHOD.CHAT)
+
+@Serializable
+data class GameResponse(val game: Game) : Response(METHOD.CREATE)
+
+
+@Serializable
+data class Game(val gameId: GameId, val balls: Int, val clients: MutableSet<ClientId> = mutableSetOf())
 
 @Serializable
 data class Message(val sender: String, val content: String, val timestamp: String)
@@ -59,45 +64,67 @@ data class Message(val sender: String, val content: String, val timestamp: Strin
 data class JoinRequest(val clientId: ClientId, val gameId: GameId)
 
 
-
 fun getUUID() = UUID.randomUUID().toString()
 
 
 val userIds = mutableListOf<ClientId>()
 val gameMap = mutableMapOf<GameId, Game>()
 
+class Connection(val session: WebSocketServerSession) {
+    companion object {
+        // TODO: maybe set UUID here
+        val lastId = AtomicInteger(0)
+    }
+
+    val name = "user${lastId.getAndIncrement()}"
+}
+
 fun Application.configureRouting() {
     routing {
+        val connections = Collections.synchronizedMap<ClientId, Connection?>(mutableMapOf())
+
         webSocket("/chat") {
+
+            val clientId = ClientId(getUUID())
+            userIds.add(clientId)
+
+            val connection = Connection(this)
+            connections[clientId] = connection
+
             try {
-
-                val clientID = ClientId(getUUID())
-                userIds.add(clientID)
-
-                sendSerialized(ClientIdTransfer(clientID))
+                sendSerialized(ClientIdResponse(clientId))
 
                 this@configureRouting.log.info("All clients connected: $userIds")
 
                 for (frame in incoming) {
-                    val data = converter?.deserialize<Transfer>(frame)!!
+                    val data = converter?.deserialize<Response>(frame)!!
                     when (data.method) {
                         METHOD.CHAT -> {
                             val message = converter?.deserialize<Message>(frame)!!
-                            sendSerialized(MessageTransfer(message))
+                            sendSerialized(MessageResponse(message))
                         }
+
                         METHOD.CREATE -> {
                             val gameId = GameId(getUUID())
                             val game = Game(gameId, 10)
                             gameMap[gameId] = game
 
-                            sendSerialized(GameTransfer(game))
+                            sendSerialized(GameResponse(game))
                         }
+
                         METHOD.JOIN -> {
                             val joinRequest = converter?.deserialize<JoinRequest>(frame)!!
                             gameMap[joinRequest.gameId]!!.clients.add(joinRequest.clientId)
 
-                            sendSerialized(GameTransfer(gameMap[joinRequest.gameId]!!))
+                            connections.forEach { (clientId, connection) ->
+                                if (gameMap[joinRequest.gameId]!!.clients.contains(clientId)) {
+                                    connection?.session?.sendSerialized(GameResponse(gameMap[joinRequest.gameId]!!))
+                                }
+                            }
+
+                            sendSerialized(GameResponse(gameMap[joinRequest.gameId]!!))
                         }
+
                         else -> {
                             this@configureRouting.log.error("Unknown method")
                         }
@@ -106,6 +133,9 @@ fun Application.configureRouting() {
 
             } catch (e: Exception) {
                 this@configureRouting.log.error("Error occurred in websocket", e)
+            } finally {
+                connections.remove(clientId)
+                userIds.remove(clientId)
             }
         }
     }
