aaed7b30f1983c0815ab343e2c180ab69e023322
RuslanZaripov <zaripovruslan864@gmail.com>
2023-04-28 12:05:41 +0300

    Add reimplemented Client structure

--- game-service/src/main/kotlin/itmo/ru/puzzle/domain/model/Client.kt
+++ game-service/src/main/kotlin/itmo/ru/puzzle/domain/model/Client.kt
@@ -1,6 +1,14 @@
 package itmo.ru.puzzle.domain.model
 
+import itmo.ru.puzzle.dto.request.CursorDTO
+import kotlinx.serialization.Serializable
+
 @JvmInline
 value class ClientId(val value: String)
 
-data class Client(val id: ClientId)
+data class Client(val id: ClientId, var cursor: Cursor?)
+
+fun Client.toDTO() = ClientDTO(id.value, cursor?.toDTO())
+
+@Serializable
+data class ClientDTO(val id: String, val cursor: CursorDTO?)
